8c5025cd821c115ef1f88525ff73e4a1c49d727e
mao <maniot.1119@gmail.com>
2025-07-24 23:52:20 +0900

    create top

--- src/views/PortForioView/index.tsx
+++ src/views/PortForioView/index.tsx
@@ -1,9 +1,24 @@
 import Header from "@/components/Header";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Link from "next/link";
 
 export function PortfolioView(){
+    const onClick=()=>{
+
+    }
     return(
         <div>
             <Header/>
+            <Typography variant="h2" align="center">
+                Welcome to My Portfolio
+            </Typography>
+            <Box>
+                初心者プログラマです。
+            </Box>
+            <Link href={"https://github.com/dashboard"}>
+                <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" width="25%"/>
+            </Link>
         </div>
     )
 }
